7ceffa9f348e1b05e1d2382475738a17fbd8900c
Orangeprofessor <Orangeprofessor17@gmail.com>
2018-10-22 12:04:06 -0400

    v1.0.1 release

--- fDumper/resource.h
+++ fDumper/resource.h
@@ -2,27 +2,10 @@
 // Microsoft Visual C++ generated include file.
 // Used by fDumper.rc
 //
-#define IDD_MAIN                        101
-#define IDR_MENU1                       103
-#define IDD_APIMENU                     108
 #define IDI_TAILS                       137
-#define IDC_CANCEL_DL                   1016
-#define IDC_START_DL                    1017
-#define IDC_PAUSE_DL                    1018
-#define IDC_RESUME_DL                   1019
-#define IDC_USERNAME                    1021
-#define IDC_UPDATE_USERNAME             1022
-#define IDC_DL_PROGRESS                 1024
-#define IDC_RATINGS                     1026
-#define IDC_GALLERIES                   1027
-#define IDC_IMAGE_LIST                  1028
-#define IDC_APIURL                      1035
-#define IDC_API_OK                      1036
-#define IDC_API_CANCEL                  1037
 #define ID_SETTINGS                     40001
 #define ID_SETTINGS_CREATEDOWNLOADFOLDER 40002
 #define ID_SETTINGS_CREATESUBGALLERYFOLDERS 40003
-#define ID_SETTINGS_CHANGEAPIADDRESS    40004
 
 // Next default values for new objects
 // 
